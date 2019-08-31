392142e0ed3dd6610ff81d24b6b8b655d7ba1a2f
sjdillon <sjdillon>
2019-08-30 22:23:04 -0400

    removed commented out lines from config

--- qthena/config.py
+++ qthena/config.py
@@ -1,7 +1,3 @@
 _CONFIG = {
-    # moved these to the event- makes no sense in a config once you think of this as a standalone moodule
-    # "db": "default",
-    # "bucket_format": "s3-{prefix1}-{prefix2}-{env}-{region}-data",
-    # "s3_query_results_path": "awsathenadata/product/queryresults",
     "sleep_seconds": 5
 }
