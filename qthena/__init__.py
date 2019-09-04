df374c37bf8a2dc1f15240c28eab650049e68f2b
sjdillon <sjdillon>
2019-09-04 16:06:46 -0400

    added class names to __init__.py imports

--- qthena/__init__.py
+++ qthena/__init__.py
@@ -1,3 +1,5 @@
-from .athena_data_reader import *
-from .boto_manager import *
-from .config import *
+from .athena_data_reader import CommandRunner
+from .boto_manager import BotoClientManager
+from .config import _CONFIG
+
+__all__ = ['BotoClientManager', 'CommandRunner', '_CONFIG']
